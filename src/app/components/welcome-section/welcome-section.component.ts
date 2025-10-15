cb048b03d5d6725094a00fa326ee18de4236e608
Nicholas <nicholas.neto@ctw-bmwgroup.com>
2025-10-15 15:09:36 +0100

    created welcome section component

---
+++ src/app/components/welcome-section/welcome-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-section',
+  imports: [],
+  templateUrl: './welcome-section.component.html',
+  styleUrl: './welcome-section.component.css',
+})
+export class WelcomeSectionComponent {}
