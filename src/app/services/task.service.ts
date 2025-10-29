8c5cb084518e345a88fe92ecafece422787a2067
Nicholas <nicholas.neto@ctw-bmwgroup.com>
2025-10-29 16:28:51 +0000

    created addTask on TaskService

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
+import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
+import { TaskStatusEnum } from '../enums/task-status.enum';
+import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamp';
 
 @Injectable({
   providedIn: 'root',
@@ -9,19 +12,39 @@ export class TaskService {
   // Tarefas todo
 
   private todoTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTask = this.todoTask$.asObservable();
+  readonly todoTask = this.todoTask$
+    .asObservable()
+    .pipe(map((tasksList) => structuredClone(tasksList)));
 
   // Tarefas in progresse
   private doingTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTask = this.doingTask$.asObservable();
+  readonly doingTask = this.doingTask$
+    .asObservable()
+    .pipe(map((tasksList) => structuredClone(tasksList)));
 
   // Tarefas done
 
   private doneTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTask = this.doneTask$.asObservable();
+  readonly doneTask = this.doneTask$
+    .asObservable()
+    .pipe(map((tasksList) => structuredClone(tasksList)));
 
-  openNewTaskModal() {
-    return '';
+  addTask(taskInfos: ITaskFormControls) {
+    const { name, description } = taskInfos;
+    const newTask: ITask = {
+      id: generateUniqueIdWithTimestamp(),
+      name,
+      description,
+      status: TaskStatusEnum.TODO,
+      comments: [],
+    };
+
+    const currenList = this.todoTask$.value;
+    return this.todoTask$.next([...currenList, newTask]);
+  }
+
+  carregarListaFontedeVerdade() {
+    console.log(' TaskService LISTA todoTask$', this.todoTask$.value);
   }
 
   openEditTaskModal() {
